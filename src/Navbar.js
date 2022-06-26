e770a87e53e2b4ba1d54fd828bf624c6f8d46bf5
Danheon <eksgjs3847@gmail.com>
2022-06-26 12:23:53 +0900

    Fix Navbar UI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,41 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import Sidebar from "./Sidebar";
+
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <nav class="Nav">
-      <h1>
-        <a href="/">danheon®</a>
-      </h1>
-      <a href="https://github.com/sjgske">
-        <img
-          className="logo"
-          src="https://simpleicons.org/icons/github.svg"
-          alt="github"
-        />
-      </a>
+    <nav className="Nav">
+      <div className="nav-container">
+        <h1>
+          <a href="/">danheon®</a>
+        </h1>
+        <div>
+          <FontAwesomeIcon
+            icon={faBars}
+            className="nav-menu-icon"
+            onClick={() => {
+              setIsActive(true);
+            }}
+          />
+          <ul className="nav-menu-list">
+            <li>
+              <a href="#project">Project</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#education">Education</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+          <Sidebar isActive={isActive} setIsActive={setIsActive} />
+        </div>
+      </div>
     </nav>
   );
 };
