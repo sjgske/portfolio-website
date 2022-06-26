c824ab7891f45bb477d37bf0dbd24176a75163ba
Danheon <eksgjs3847@gmail.com>
2022-06-26 12:23:07 +0900

    Refactor code

--- src/Main.js
+++ src/Main.js
@@ -11,13 +11,13 @@ const Main = () => {
           김단헌입니다.
         </h1>
         <p>
-          사용자들에게 좋은 인상을 주고 싶은 <strong>프론트엔드 개발자</strong>
+          사용자들에게 좋은 인상을 주고 싶은 <strong>프론트엔드 개발자 </strong>
           김단헌입니다. 인터랙티브하고 이용하기 편한 UI를 설계하는 것에 관심이
           많습니다.
         </p>
       </header>
 
-      <section className="section project">
+      <section id="project" className="section project">
         <h1>
           <FontAwesomeIcon icon={faCode} className="icon" />
           Project
@@ -25,18 +25,18 @@ const Main = () => {
         <div>
           <h3>개인</h3>
           <div className="item">
-            <h4>OO프로젝트</h4>
+            <h4>감정 일기장</h4>
           </div>
         </div>
         <div>
           <h3>팀</h3>
           <div className="item">
-            <h4>OO프로젝트</h4>
+            <h4>쇼핑몰 프로젝트</h4>
           </div>
         </div>
       </section>
 
-      <section className="section skills">
+      <section id="skills" className="section skills">
         <h1>
           <FontAwesomeIcon icon={faCode} className="icon" />
           Skills
@@ -48,32 +48,36 @@ const Main = () => {
         </ul>
       </section>
 
-      <section className="section education">
+      <section id="education" className="section education">
         <h1>
           <FontAwesomeIcon icon={faCode} className="icon" />
           Education
         </h1>
         <div className="item">
           <h4>엘리스 SW 엔지니어 트랙 2기</h4>
-          2022년 4월 4일 ~ 7월 22일
+          <span>2022년 4월 4일 ~ 7월 22일</span>
         </div>
-        <div className="item">
+        {/* <div className="item">
           <h4>전북대학교 신소재공학부</h4>
-          2017년 3월 ~ 2022년 2월
-        </div>
+          <span>2017년 3월 ~ 2022년 2월</span>
+        </div> */}
       </section>
 
-      <section className="section contact">
+      <section id="contact" className="section contact">
         <h1>
           <FontAwesomeIcon icon={faCode} className="icon" />
           Contact
         </h1>
         <ul>
           <li>
-            <a href="mailto:eksgjs3847@gmail.com">Email</a>
+            <a className="link" href="mailto:eksgjs3847@gmail.com">
+              Email
+            </a>
           </li>
           <li>
-            <a href="https://github.com/sjgske">Github</a>
+            <a className="link" href="https://github.com/sjgske">
+              Github
+            </a>
           </li>
         </ul>
       </section>
--- src/TopButton.js
+++ src/TopButton.js
@@ -2,28 +2,30 @@ import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleUp } from "@fortawesome/free-solid-svg-icons";
 
-const TopButton = () => {
-  const [isActive, setIsActive] = useState(false);
+const useScroll = (initialValue) => {
+  const [isActive, setIsActive] = useState(initialValue);
 
-  const useScroll = () => {
-    const listener = () => {
-      if (window.pageYOffset > 30) {
-        setIsActive(true);
-      } else {
-        setIsActive(false);
-      }
-    };
-
-    useEffect(() => {
-      window.addEventListener("scroll", listener);
-      return () => {
-        window.removeEventListener("scroll", listener);
-      };
-    });
+  const listener = () => {
+    window.pageYOffset > 30 ? setIsActive(true) : setIsActive(false);
   };
 
-  useScroll();
+  // useEffect 안에서 setState를 부르면 무한 실행.. X
+  // useEffect 안에서 함수를 리턴하면 unmount 시점에 실행됨
+  useEffect(() => {
+    window.addEventListener("scroll", listener);
+    return () => {
+      window.removeEventListener("scroll", listener);
+    };
+  });
+  return { isActive };
+};
 
+useScroll.defaultProps = {
+  initialValue: false,
+};
+
+const TopButton = () => {
+  const { isActive } = useScroll(false);
   return (
     <div
       id="TopButton"
