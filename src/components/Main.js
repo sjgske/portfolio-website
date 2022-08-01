fc5ee68a6d02c066c57f537b5fed1f1cb249ad5f
Danheon <eksgjs3847@gmail.com>
2022-08-01 18:51:34 +0900

    💬 Add new project content

--- src/components/Main.js
+++ src/components/Main.js
@@ -23,8 +23,63 @@ const Main = () => {
           <FontAwesomeIcon icon={faCode} className="icon" />
           Project
         </h1>
+
         <div>
           <h3>팀</h3>
+          <div className="item">
+            <div className="title">
+              <h4>
+                <a
+                  href="https://github.com/sjgske/elice-hea17h-service"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  hea17h 식단관리 서비스
+                </a>
+              </h4>
+              <span>2022년 7월 4일 ~ 22일</span>
+              <div>
+                <Tag>React</Tag>
+                <Tag>Redux-toolkit</Tag>
+                <Tag>Styled-Components</Tag>
+                <Tag>Git</Tag>
+              </div>
+            </div>
+            <div>
+              <p>
+                프론트엔드를 담당하였고,
+                <strong> 효율적인 디자인 시스템 관리</strong>를 위해
+                <code>React</code> 프레임워크로 개발하였습니다.
+              </p>
+              <p>
+                식단 목록 페이지를 구현하였고, <strong>UX</strong>와
+                <strong> 서비스 속도</strong>를 개선하기 위해
+                <code className="kor"> 날짜별 필터링 기능</code>을
+                추가하였습니다.
+              </p>
+              <p>
+                <code>REST API</code>와의 비동기 통신으로 데이터를 가공하여
+                컴포넌트에 전달하였습니다.
+              </p>
+              <p>
+                Props drilling을 방지하기 위해 <code>Redux-toolkit</code>으로
+                상태를 관리하였습니다.
+              </p>
+              <p>
+                컴포넌트 리렌더링을 막기위해 <code>useMemo</code>,
+                <code>useCallback</code>으로 최적화하였습니다.
+              </p>
+              <p>
+                CSS in JS의 장점인 <strong>dynamic한 컴포넌트 개발</strong>을
+                위해 <code>Styled-Components</code>로 스타일링하였습니다.
+              </p>
+
+              <p>
+                <code>eslint</code>, <code>prettier</code>로 코드 컨벤션을
+                관리하였습니다.
+              </p>
+            </div>
+          </div>
           <div className="item">
             <div className="title">
               <h4>
@@ -56,9 +111,8 @@ const Main = () => {
               </p>
               <p>
                 <code className="kor">로컬스토리지</code>에 JWT 토큰을 저장하는
-                로그인 기능을 통해
-                <br />
-                로그인 상태에 따라 다르게 그려지는 UI를 구현하였습니다.
+                로그인 기능을 통해 로그인 상태에 따라 다르게 그려지는 UI를
+                구현하였습니다.
               </p>
               <p>
                 <strong>다음 주소 api</strong>를 이용하여 사용자 정보 수정 폼을
@@ -74,6 +128,7 @@ const Main = () => {
             </div>
           </div>
         </div>
+
         <div>
           <h3>개인</h3>
           <div className="item">
@@ -95,12 +150,12 @@ const Main = () => {
             </div>
             <div>
               <p>
-                <strong>리액트 상태관리 hook</strong>을 활용하여 글을 동적으로{" "}
-                <strong>추가, 수정, 삭제</strong>하는 기능을 구현하였습니다.
+                <strong>리액트 상태관리 hook</strong>을 활용하여 글을 동적으로
+                <strong> 추가, 수정, 삭제</strong>하는 기능을 구현하였습니다.
               </p>
               <p>
-                버튼과 헤더 UI를 구현하고{" "}
-                <code className="kor">컴포넌트화</code>
+                버튼과 헤더 UI를
+                <code className="kor"> 컴포넌트화</code>
                 하여 재사용성을 향상시켰습니다.
               </p>
             </div>
@@ -155,6 +210,8 @@ const Main = () => {
           <li>HTML / CSS</li>
           <li>JavaScript</li>
           <li>React.js</li>
+          <li>Typescript</li>
+          <li>CSS in JS</li>
           <li>Git</li>
         </ul>
       </section>
@@ -168,10 +225,10 @@ const Main = () => {
           <h4>엘리스 SW 엔지니어 트랙 2기</h4>
           <span>2022년 4월 4일 ~ 7월 22일</span>
         </div>
-        {/* <div className="item">
+        <div className="item">
           <h4>전북대학교 신소재공학부</h4>
           <span>2017년 3월 ~ 2022년 2월</span>
-        </div> */}
+        </div>
       </section>
 
       <section id="contact" className="section contact">
